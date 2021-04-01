5c516dfdda96af085ebd4895f2244f6d2a4851ba
cartyad <cartyad@tcd.ie>
2021-04-02 00:43:46 +0100

    Update Script_1.py
    File path Working

--- Script_1.py
+++ Script_1.py
@@ -1,5 +1,6 @@
 #https://www.youtube.com/watch?v=H71ts4XxWYU
 
+
 import tkinter as tk
 import pandas as pd
 from tkinter import filedialog
@@ -12,21 +13,18 @@ app = tk.Tk() # the application itself
 app.title("Test") # title of window
 
 def selectFile():
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
-    if file_path != "":
-        return file_path
-    elif file_path == "":
-        file_path=-1
-        return file_path
+    X = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
+    print(X)
+    file_path = X
+    file_pathlabel = Label(app, text=file_path) # creates label
+    file_pathlabel.pack() # adds the label to the window
+    return file_path
 
 label = Label(app, text="Testing testing one, two, three") # creates label
 label.pack() # adds the label to the window
 button = tk.Button(app, text="Select a .csv, .json or .json-text file.", width = 50, command = lambda: file_path == selectFile())
 button.pack()
-if file_path != -1:
-    print(file_path)
-    #file_pathlabel = Label(app, text=file_path) # creates label
-    #file_pathlabel.pack() # adds the label to the window
+
 
 app.mainloop() # this must go at the end of your window code
 
