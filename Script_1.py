2525e8edab4b3cdbc895cd3cc2e8087d1662e78b
cartyad <cartyad@tcd.ie>
2021-04-01 23:45:54 +0100

    Update Script_1.py

--- Script_1.py
+++ Script_1.py
@@ -4,6 +4,14 @@ import tkinter as tk
 import pandas as pd
 from tkinter import filedialog
 
+
+app = Tk() # the application itself
+app.title("Test") # title of window
+
+label = Label(app, text="Testing testing one, two, three") # creates label
+label.pack() # adds the label to the window
+
+app.mainloop() # this must go at the end of your window code
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
