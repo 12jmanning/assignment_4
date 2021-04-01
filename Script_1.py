85ac3506139a85acb06ea592e852338d093ba56f
cartyad <cartyad@tcd.ie>
2021-04-01 15:03:22 +0100

    Script_1 part 1 Complete
    Allows user to select csv, json and json-stat files from the gui

--- Script_1.py
+++ Script_1.py
@@ -1,1 +1,15 @@
 # testing testing
+
+#https://www.youtube.com/watch?v=H71ts4XxWYU
+
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
+
+print(file_path)
+
+input('Press any key to exit')
+
