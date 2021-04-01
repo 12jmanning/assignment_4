2b1f3c8c1393f6a89576418d748c256a0c9837b9
cartyad <cartyad@tcd.ie>
2021-04-01 15:17:00 +0100

    Update Script_1.py
    Handling various file inputs

--- Script_1.py
+++ Script_1.py
@@ -3,6 +3,7 @@
 #https://www.youtube.com/watch?v=H71ts4XxWYU
 
 import tkinter as tk
+import pandas as pd
 from tkinter import filedialog
 
 root = tk.Tk()
@@ -10,6 +11,14 @@ root.withdraw()
 file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
 
 print(file_path)
+csv=".csv"
+json = ".json"
+json_stat = ".json-stat"
 
 input('Press any key to exit')
 
+if file_path!="": 
+    if file_path.find(csv)!=-1:
+        df= df = pd.read_csv(file_path)
+
+print(df)
