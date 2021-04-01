c9068c371add52fa002c1b3c69b7139862a57b56
cartyad <cartyad@tcd.ie>
2021-04-01 15:43:08 +0100

    Update Script_1.py

--- Script_1.py
+++ Script_1.py
@@ -3,14 +3,20 @@
 #https://www.youtube.com/watch?v=H71ts4XxWYU
 
 import tkinter as tk
-import pandas as pd
+#import pandas as pd
+from tkinter import *
 from tkinter import filedialog
+
+#file_path = ""
 
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
 
-print(file_path)
+#fileOpen()
+#button = Button(text="Select a .csv, .json or .json-text file.", width = 50, command= fileOpen).pack()
+#a.mainloop()
+
 csv=".csv"
 json = ".json"
 json_stat = ".json-stat"
@@ -19,6 +25,6 @@ input('Press any key to exit')
 
 if file_path!="": 
     if file_path.find(csv)!=-1:
-        df= df = pd.read_csv(file_path)
+        df= df = file_path # pd.read_csv(file_path)
 
 print(df)
