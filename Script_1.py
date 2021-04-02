e0a687392509219d0757ca6c24bd393d7bd079af
cartyad <cartyad@tcd.ie>
2021-04-02 01:26:14 +0100

    Update Script_1.py
    late shift trying to get the filepath working properly

--- Script_1.py
+++ Script_1.py
@@ -5,35 +5,47 @@ import tkinter as tk
 import pandas as pd
 from tkinter import filedialog
 from tkinter import *
+import pandas as pd
 
-global file_path
-file_path = -1 
+
 
 app = tk.Tk() # the application itself
 app.title("Test") # title of window
 
+
+
 def selectFile():
+    
     X = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
-    print(X)
+    #print(X)
     file_path = X
     file_pathlabel = Label(app, text=file_path) # creates label
     file_pathlabel.pack() # adds the label to the window
+    #print(file_path)
     return file_path
 
 def data_finder(fp):
+    data=-1
     if fp.lower().endswith(".json"):
-        xyz
+        x = pd.read_json(fp)
+        data = pd.DataFrame(data=x)
     elif fp.lower().endswith(".csv"):
-        xyz
+        y = pd.read_csv(fp)
+        data = pd.DataFrame(data=y)
     elif fp.lower().endswith(".json-stat"):
-        xyz
+        x=-1
+        data=x
+    return data
 
+file_path = StringVar()
 label = Label(app, text="Testing testing one, two, three") # creates label
 label.pack() # adds the label to the window
 button = tk.Button(app, text="Select a .csv, .json or .json-text file.", width = 50, command = lambda: file_path == selectFile())
 button.pack()
-
-data = data_finder(file_path)
+#data_path=file_path
+print(file_path)
+#data = data_finder(file_path)
+#print(data)
 
 app.mainloop() # this must go at the end of your window code
 
