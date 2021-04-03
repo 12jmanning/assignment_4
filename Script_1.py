e57e334a99712e68c948bdafe0096a2d8afbd42d
cartyad <cartyad@tcd.ie>
2021-04-04 00:06:32 +0100

    Update Script_1.py
    Woeking version, shows data details and allows export of data to excel. Filepath for new excel sheet tbd

--- Script_1.py
+++ Script_1.py
@@ -19,8 +19,12 @@ def selectFile():
     X = filedialog.askopenfilename(filetypes=[("Image files", "*.csv; *.json; *.json-stat")])
     #print(X)
     file_path = X
+    label_1 = Label(app, text="The file's location that you have selected is as follows:") # other option
+    label_1.pack()
     file_pathlabel = Label(app, text=file_path) # creates label
     file_pathlabel.pack() # adds the label to the window
+    label_s = Label(app, text="") # other option
+    label_s.pack()
     #print(file_path)
     data = data_finder(file_path)
 
@@ -37,12 +41,58 @@ def data_finder(fp):
     elif fp.lower().endswith(".json-stat"):
         x=-1
         data=x
-    data.head()
-    print(list(data.columns.values.tolist()))
+    #data.head()
+    #print(list(data.columns.values.tolist()))
+    #label_3 = Label(app, text=str(data.head())) # other option
+    #label_3.pack()
+    label_2 = Label(app, text="The first 5 rows of the dataset are as follows:") # other option
+    label_2.pack()
+    label_3 = Label(app, text=str(data.iloc[:6,:data.shape[1]])) # other option
+    label_3.pack()
+    dataSetInfo(data)
     return data
 
+def dataSetInfo(data):
+    label_s = Label(app, text="") # other option
+    label_s.pack()
+    label_t = Label(app, text="The number of rows in the dataset is:") # other option
+    label_t.pack()
+    label_1 = Label(app, text=str(data.shape[0])) # other option
+    label_1.pack()
+
+    label_s2 = Label(app, text="") # other option
+    label_s2.pack()
+    label_t2 = Label(app, text="The number of columns in the dataset is:") # other option
+    label_t2.pack()
+    label_2 = Label(app, text=str(data.shape[1])) # other option
+    label_2.pack()
+
+    x = list(data.columns.values.tolist())
+    string_1 = ""
+    for i in x:
+        string_1= string_1 + "\n" + i
+
+    label_s3 = Label(app, text="") # other option
+    label_s3.pack()
+    label_t3 = Label(app, text="The columns of the dataset are:") # other option
+    label_t3.pack()
+    label_3 = Label(app, text=string_1) # other option
+    label_3.pack()
+
+    label_s4 = Label(app, text="") # other option
+    label_s4.pack()
+    label_t4 = Label(app, text="Please click the button below to export to an excel file:") # other option
+    label_t4.pack()
+    button_2 = tk.Button(app, text="Export Data",  command = lambda: convertToExcel(data))
+    button_2.pack()
+
+
+def convertToExcel(data):
+    data.to_excel()
+
+
 file_path = StringVar()
-label = Label(app, text="Testing testing one, two, three") # creates label
+label = Label(app, text="Please click the button below to select a data file:") # creates label
 label.pack() # adds the label to the window
 button = tk.Button(app, text="Select a .csv, .json or .json-text file.", width = 50, command = lambda: file_path == selectFile())
 button.pack()
