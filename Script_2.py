e3c4fb84b1fbdd145a26c92259badda41d484c6c
cartyad <cartyad@tcd.ie>
2021-04-15 13:04:48 +0100

    Accomodation csv File

--- Script_2.py
+++ Script_2.py
@@ -1,10 +1,27 @@
 import csv, sys
+import urllib3
+import tkinter as tk
+import pandas as pd
+from tkinter import filedialog
+from tkinter import *
+import pandas as pd
+import numpy as np
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
 
-filename = 'some.csv'
-with open(filename, 'rb') as f:
-    reader = csv.reader(f)
-    try:
-        for row in reader:
-            print(row)
-    except csv.Error as e:
-        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+Attractions = pd.read_csv("Attractions.csv")
+Attractions = pd.DataFrame(data=Attractions)
+print(Attractions)
+
+Accommodation = pd.read_csv("Accommodation.csv")
+Accommodation = pd.DataFrame(data=Accommodation)
+print(Accommodation)
+
+Activities = pd.read_csv("Activities.csv")
+Activities = pd.DataFrame(data=Activities)
+print(Activities)
+
+print("next")
+df = pd.merge(Accommodation, Attractions,how='inner', on='AddressRegion')
+print(df)
