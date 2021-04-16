3e24559dea7b4a1d12c69a11e33ea15d2bb7dc25
cartyad <cartyad@tcd.ie>
2021-04-16 10:54:30 +0100

    Update Script_2.py

--- Script_2.py
+++ Script_2.py
@@ -25,6 +25,38 @@ Activities = pd.read_csv("Activities.csv")
 Activities = pd.DataFrame(data=Activities)
 
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='My First Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+    
+if __name__ == '__main__':
+    app.run_server(debug=True, use_reloader=False)
+
+
 
 #df = pd.merge(Accommodation, Attractions,how='inner', on='AddressRegion')
 #df = Accommodation.merge(Attractions, on = 'AddressRegion').merge(Activities, on = 'AddressRegion')
